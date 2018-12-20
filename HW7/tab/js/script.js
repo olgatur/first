3def4d0f5af29cd7e097d28fad72c079545b3c2d
Olya <olchik-0411@rambler.ru>
2018-12-20 08:59:02 +0300

    8 lesson

---
+++ HW7/tab/js/script.js
@@ -0,0 +1,82 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    //Эта функция скрывает все tabContent со страницы
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        } 
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+    
+            for (let i = 0; i < tab.length; i++){
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }    
+        }
+    });
+
+    // Timer
+
+    let deadline = '2018-11-21'; //эту дату можно ввести с сервера или спрашивать у пользователя 
+
+    // Через функцию узнаем промежуток времени до дедлайна 
+    // И вычленяем полностью время, часы, минуты, секунды
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    // Пишем функцию, которая превращает статичную верстку в динамическую
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+    
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+    } 
+
+    setClock('timer', 'deadline');
+
+});
