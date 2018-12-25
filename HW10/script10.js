0a1df44d97e1f8bbd2bb6e5f39191df72c4471ce
Olya <olchik-0411@rambler.ru>
2018-12-25 21:10:22 +0300

    10 lesson

--- HW10/script10.js
+++ HW10/script10.js
@@ -1,24 +1,56 @@
 "use strict";
 
 class Options{
-    constructor(height, width, bg, fontSize, textAlign){
+    constructor(height, width = 100, bg = 'red', fontSize = 16, textAlign = 'center') {
         this.height = height;
         this.width = width;
         this.bg = bg;
         this.fontSize = fontSize;
         this.textAlign = textAlign;
-    }
-    methodDiv(){
+        }
+        cssText: function () {
+            
+                let newDiv = document.createElement('div');
+       // div.classList.add('cssText');
+        newDiv.innerHTML = "<h1>Hello!</h1>";
+            }
+            
+    };
+    cssText();
+   
+        
+        const cst = new Options(50, 100, 'red', 86, 'center');
+    div.wDiv();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //console.log(wDiv());
+    //newDiv.classList
+        //let squ = new Options (50, 100, 'red', 16, 'center', 'exit()');
+        //console.log(squ);
+        
+  
+   //newDiv.wDiv();
+    //create() {
 
-    }
-
-    let obj = {
-        height: 50,
-        width: 100,
-        bg: 'red',
-        fontSize: '16px',
-        textAlign: 'center'
-    } 
-};
-
-let square = new Option(10, 10, 'red', 16, 'center');
+    //let newDiv = document.createElement('div');
+        //newDiv.classList.add('my-class');
+        
+ 
+    
