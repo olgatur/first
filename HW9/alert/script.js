d2f70f46b4cca66e9cb3ac21aab6e41f95dd1096
Olya <olchik-0411@rambler.ru>
2018-12-25 03:13:34 +0300

    10

--- HW9/alert/script.js
+++ HW9/alert/script.js
@@ -3,7 +3,9 @@
 let age = document.getElementById('age');
   
 function showUser(surname, name) {
-    alert("Пользователь " + surname + " " + name + ", его возраст " + age.value);
+    //document.write("Пользователь " + surname + " " + name + ", его возраст " + age.value);
+    document.write(`Пользователь ${surname} ${name} , его возраст ${age.value}`);
+
 }
  
 showUser("Snow", "Alex", age);
